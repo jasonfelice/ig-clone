53af83bda77979917031310626d1f2cd0031e8cc
Jake Felice <jasonfelice0@gmail.com>
2022-11-21 06:47:38 -0800

    Create and style post component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Post from './components/Post';
 
 function App() {
   return (
@@ -6,6 +7,11 @@ function App() {
       <div className="app__header">
         <img src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt="" className="app__headerLogo" />
       </div>
+      <Post
+        username = "Jake"
+        imageUrl = "https://onlinecoursetutorials.com/wp-content/uploads/2022/02/what-is-reactjs-advantages-and-disadvantages.png"
+        description = "Here goes the description"
+      />
     </div>
   );
 }
---
+++ src/components/Post.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Post.css';
+import { Avatar } from '@mui/material';
+
+function Post({username, imageUrl, description}) {
+  return (
+    <div className="post">
+        <div className="post__header">
+          <Avatar
+            className="post__avatar"
+            src="static/images/avatar/1.jpg"
+          />
+          <h3>{username}</h3>
+        </div>
+
+        <img className="post__image" src={imageUrl} alt="" />
+
+        <h4 className="post__description"><strong>{username}</strong>: {description}</h4>
+    </div>
+  )
+}
+
+export default Post
