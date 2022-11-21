4748356981ad6407b674e7b7ded2a9dfcf61c00a
Jake Felice <jasonfelice0@gmail.com>
2022-11-21 07:05:58 -0800

    Set state for posts

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
+import React, { useState } from 'react';
 import './App.css';
 import Post from './components/Post';
 
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      username: "Jake",
+      imageUrl: "https://onlinecoursetutorials.com/wp-content/uploads/2022/02/what-is-reactjs-advantages-and-disadvantages.png",
+      description: "Here goes the description",
+    },
+    {
+      username: "Sam",
+      imageUrl: "https://onlinecoursetutorials.com/wp-content/uploads/2022/02/what-is-reactjs-advantages-and-disadvantages.png",
+      description: "Here same's description",
+    },
+  ]);
   return (
     <div className="app">
       <div className="app__header">
         <img src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt="" className="app__headerLogo" />
       </div>
-      <Post
-        username = "Jake"
-        imageUrl = "https://onlinecoursetutorials.com/wp-content/uploads/2022/02/what-is-reactjs-advantages-and-disadvantages.png"
-        description = "Here goes the description"
-      />
+      {
+        posts.map((post) => (
+          <Post
+            username = {post.username}
+            imageUrl = {post.imageUrl}
+            description = {post.description}
+          />
+        ))
+      }
     </div>
   );
 }
