4199d46c3855659ddd2d8410ced94c3a49564f0d
Jake Felice <jasonfelice0@gmail.com>
2022-11-28 07:34:53 -0800

    Set up login splash screen

--- src/pages/Splash.js
+++ src/pages/Splash.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React from 'react';
+import './Splash.css';
 
 export default function Splash() {
   return (
     <section>
         <main>
-            Splash
+            <div className="splash__login">
+              <form>
+                <input type="email" name="email" placeholder="Email"/>
+                <input type="password" name="password" placeholder="Password" />
+                <input type="submit" value="Log in" />
+              </form>
+            </div>
+            <div className="splash__prompt">
+              Don't have an account? Sign up
+            </div>
         </main>
         <footer>
             footer
