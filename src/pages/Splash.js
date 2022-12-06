06996a576edc53d38b4594b2c0341daac69d39a3
Jake Felice <jasonfelice0@gmail.com>
2022-12-06 00:29:31 -0800

    Set up sign in authentication

--- src/pages/Splash.js
+++ src/pages/Splash.js
@@ -2,19 +2,37 @@ import { React, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Splash.css';
 import logo from '../assets/logo.png';
+import spinner from '../assets/spinner.gif';
+import { auth } from '../fire';
+import { signInWithEmailAndPassword } from "firebase/auth";
 
 export default function Splash() {
   const [form, setForm] = useState({
     email: "",
     password: ""
   });
-  const formValid = !!(form.email && form.password.length > 3);
+  const [loading, setLoading] = useState(false);
+  const {email, password } = form;
+  const formValid = !!(email && password.length > 3);
+
   const handleInput = (e) => {
     setForm({
       ...form,
       [e.target.name]: e.target.value
     });
-  }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    signInWithEmailAndPassword(auth, email, password)
+    .then(() => setLoading(false))
+    .catch((error) => {
+      setLoading(false);
+      const errorCode = error.code;
+      const errorMessage = errorMessage;
+    })
+  };
   return (
     <section>
         <main>
@@ -24,9 +42,13 @@ export default function Splash() {
                   <img src={logo} alt="Logo" className="splash__logo" />
                 </div>
                 <form>
-                  <input onChange={handleInput} value={form.email} type="email" name="email" placeholder="Email"/>
-                  <input onChange={handleInput} value={form.password} type="password" name="password" placeholder="Password" />
-                  <button disabled={!formValid} type="submit">Log in </button>
+                  <input onChange={handleInput} value={email} type="email" name="email" placeholder="Email"/>
+                  <input onChange={handleInput} value={password} type="password" name="password" placeholder="Password" />
+                  <button onClick={handleSubmit} disabled={(!formValid || loading)} type="submit">
+                    {
+                      loading ? (<img src={spinner} alt="spinner" />) : "Log in"
+                    }
+                  </button>
                 </form>
               </div>
               <div className="splash__prompt">
