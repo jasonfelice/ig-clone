245a3a63dfb37f4c8bcb9d63394918ae4816204e
Jake Felice <jasonfelice0@gmail.com>
2023-01-06 01:38:42 -0800

    Change double quotes to single quotes in Posts

--- src/pages/Posts.js
+++ src/pages/Posts.js
@@ -1,12 +1,12 @@
-import React, { useState, useEffect } from "react";
-import { collection, getDocs, orderBy, query } from "firebase/firestore"; 
-import { db } from "../fire";
-import Post from "../components/Post";
+import React, { useState, useEffect } from 'react';
+import { collection, getDocs, orderBy, query } from 'firebase/firestore'; 
+import { db } from '../fire';
+import Post from '../components/Post';
 
 export default function Posts({ user }) {
   const [posts, setPosts] = useState([]);
   const getPosts = async () => {
-    const querySnapshot = await getDocs(query(collection(db, "posts"), orderBy("timestamp", "desc")));
+    const querySnapshot = await getDocs(query(collection(db, 'posts'), orderBy('timestamp', 'desc')));
     setPosts(querySnapshot.docs.map(doc => (
         {
           id: doc.id,
